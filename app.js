5e82db1060f5e082d1914eb8c659f61fac9ebd53
thislifehyeon <vkhkhv@naver.com>
2021-11-26 10:21:59 -0800

    Jest Test

--- app.js
+++ app.js
@@ -2,28 +2,24 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
+const handlers = require('./lib/handlers')
+
+const fortune = require('./lib/fortune')
 
 app.engine('handlebars', engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => res.render('home'))
+app.use(express.static(__dirname + '/public'))
 
-app.get('/about', (req, res) => res.render('about'))
+app.get('/', handlers.home)
+
+app.get('/about', handlers.about)
 
 //app.use는 미들웨어와 관련이 있는 메서드.
 //라우트가 일치하지 않을 경우 사용할 폴백 핸들러.
-app.use((req,res) => {
-    res.type('text/plain')
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
-app.use((err, req, res, next) => {
-    console.err(err.message)
-    res.type('text/plain')
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port};` +
