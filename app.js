9ec273ab31bc05635d858f3a51bab4c0ef465d51
thislifehyeon <vkhkhv@naver.com>
2021-11-29 01:17:03 -0800

    테스트추가, 린트 추가

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use(handlers.notFound)
 
 app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-    `Express started on http://localhost:${port};` +
-    `press Ctrl-C to terminate`))
+if(require.main === module){
+    app.listen(port, () => console.log(
+        `Express started on http://localhost:${port};` +
+        `press Ctrl-C to terminate`))
+}
+else {
+    module.exports = app
+}
